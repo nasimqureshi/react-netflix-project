19dd00f16618321630a9cae8ced4778148aff1dd
nasimqureshi <qureshi.nasim@yahoo.com>
2021-01-29 10:43:31 -0600

    first commit

---
+++ netflix/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function App(){
+    //new Date(year, month, hours, minutes, seconds, milliseconds)
+    let currDate = new Date (20, 2, 5, 15);
+    currDate = currDate.getHours();
+    
+}
