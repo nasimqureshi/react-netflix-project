6bb4108236837178244d3bb59ff26f90749eb349
nasimqureshi <qureshi.nasim@yahoo.com>
2021-01-31 11:43:26 -0600

    sixth commit

--- netflix/src/index.js
+++ netflix/src/index.js
@@ -29,13 +29,13 @@ title="A Netflix Original Series"
 link="https://www.netflix.com/in/title/80990668?source=35"
  />
 
-<Card  imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-title="A Netflix Original Series"
- sname='Extra Curriclar' 
+<Card  imgsrc="https://www.hitc.com/static/uploads/htcn/1816/extracurricular_netflix_780_.jpg"
+title="extra curricular"
+ sname='extra curricular' 
 link="https://www.netflix.com/in/title/80990668?source=35"/>
 
-<Card  imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-title="A Netflix Original Series"
+<Card  imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
+title="Stranger Thins"
  sname='Stranger Things' 
 link="https://www.netflix.com/in/title/80990668?source=35"/>
  </>,
