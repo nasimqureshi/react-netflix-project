8db37f20bda6a9b70248c0d676d42dd6bb573e57
nasimqureshi <qureshi.nasim@yahoo.com>
2021-01-30 22:23:42 -0600

    fourth commit

--- netflix/src/index.js
+++ netflix/src/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+function Card(props){
+  return(
+    
+<div className="cards">
+  <div className="card">
+    <img src={props.imgsrc} alt="myPic" className="card_img" />
+    <div className="card__info">
+      <span className="card__category">A Netflix original Series</span>
+      <h3 className="card__title">DARK</h3>
+      <a href="" target="_blank">
+        <button>Watch Now</button>
+      </a>
+    </div>
+  </div>
+    
+</div>
+
+  )
+}
 
 
 ReactDOM.render(
   <>
-    <div className="cards">
-      <div className="card">
-        <img src="https://wallpapercave.com/wp/wp4056410.jpg" alt="myPic" className="card_img" />
-        <div className="card__info">
-          <span className="card__category">A Netflix original Series</span>
-          <h3 className="card__title">DARK</h3>
-          <a href="https://www.netflix.com/in/title/80990668?source=35" target="_blank">
-            <button>Watch Now</button>
-          </a>
-        </div>
-      </div>
-        
-    </div>
-
-  </>,
+<Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+title="A Netflix Original Series"
+ sname='DARK' 
+link="https://www.netflix.com/in/title/80990668?source=35"
+ />
+<Card />
+<Card />
+ </>,
   document.getElementById('root')
 );
 
